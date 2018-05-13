8c13286424bec08b730d2b4877074834e9475af2
Alex Shink <alshinkareff@ya.ru>
2018-05-14 00:39:50 +0700

    last autonomous move

--- resources/common.js
+++ resources/common.js
@@ -1,76 +1,74 @@
 $(document).ready(function(){
 
-  var moveKeys = [37, 38, 39, 40];
+  ws = new WebSocket('ws://api.dao.ping1111.com:3000/websocket');
+  var wsData;
 
-  function move(eClass, distance, jump){
-  // TRANSLATE MATRIX MOVE
-    // var matrix = $.map(
-    //   $('.' + eClass).css('transform').slice(7, -1).split(', '), Number
-    // );
-    // if ( jump !== undefined ) {
-    //   var matrixJump = [];
-    //   for ( var i=0; i<matrix.length; i++ ) {
-    //     matrixJump.push(matrix[i]);
-    //   };
-    //   matrixJump[5] -= distance;
-    //   $('.' + eClass).css('transform', 'matrix(' + matrixJump.join(', ') + ')');
-    //   setTimeout(function(){
-    //     matrix[5] = 0;
-    //     $('.' + eClass).css('transform', 'matrix(' + matrix.join(', ') + ')');
-    //   }, 150)
-    // } else {
-    //   matrix[4] += distance;
-    //   $('.' + eClass).css('transform', 'matrix(' + matrix.join(', ') + ')');
-    // }
+  ws.onopen = function(){
+    console.log('Соединение установлено');
+    ws.send('reset');
+    Game();
+  };
 
-  // ABSOLUTE MOVE
-    if ( jump !== undefined ) {
-      $('.' + eClass)
-        .addClass('daohero_jump')
-        .css('bottom', '+=' + distance);
-      setTimeout(function(){
-        $('.' + eClass).css('bottom', '0' )
-      }, 200)
-    } else {
-      $('.' + eClass).addClass('daohero_run');
-      if ( keyPressed == 39 ) {
+  ws.onmessage = function(e){
+    console.log('Получены данные: ' + e.data);
+    wsData = JSON.parse(e.data);
+    // reset
+    // $('.daohero').css('left', wsData.x)
+    console.log('Игра запущена');
+  };
+
+  function Game(){
+
+    var moveKeys = [37, 38, 39, 40];
+
+    function move(eClass, distance, jump){
+
+    // ABSOLUTE MOVE
+      if ( jump !== undefined && $('.' + eClass).css('bottom').replace('px', '') == 0) {
+        $('.' + eClass)
+          .addClass('daohero_jump')
+          .css('bottom', '+=' + distance);
+        setTimeout(function(){
+          $('.' + eClass).css('bottom', '0' )
+        }, 200)
+      } else if ( keyPressed == 39 ) {
+        $('.' + eClass).addClass('daohero_run');
+        $('.' + eClass).css('left', '+=' + distance);
         $('.' + eClass).addClass('daohero_run_right');
-      } else {
+      } else if ( keyPressed == 37 ) {
+        $('.' + eClass).addClass('daohero_run');
+        $('.' + eClass).css('left', '+=' + distance);
         $('.' + eClass).addClass('daohero_run_left');
       }
-      $('.' + eClass).css('left', '+=' + distance);
-    }
-  }
-
-  $(document).bind('keydown', function(e){
-    if ( $.inArray(e.keyCode, moveKeys) !== -1 ) {
-      console.log('Отправим через ws: номер нажатой стрелки: ' + e.keyCode);
-      
-      window.keyPressed = e.keyCode;
-
-      switch(e.keyCode) { 
-        case 39:
-          move('daohero', 60);
-          break
-
-        case 37:
-          move('daohero', -60);
-          break
-
-        case 38:
-          move('daohero', 60, 1);
-          break
-
-        case 40:
-          break
       }
-    }
-  });
 
-  $(document).keyup(function(e) {   
-     if ( $.inArray(e.which, moveKeys) !== -1 ){
-        $('.daohero').removeClass('daohero_jump daohero_run daohero_run_left daohero_run_right')
-     }
-  });
+    $(document).bind('keydown', function(e){
+      if ( $.inArray(e.keyCode, moveKeys) !== -1 ) {
+        
+        window.keyPressed = e.keyCode;
+
+        switch(keyPressed) { 
+          case 39:
+            move('daohero', 60);
+            break
+
+          case 37:
+            move('daohero', -60);
+            break
+
+          case 38:
+            move('daohero', 60, 1);
+            break
+        }
+      }
+    });
+
+    $(document).keyup(function(e) {   
+       if ( $.inArray(e.which, moveKeys) !== -1 ){
+          $('.daohero').removeClass('daohero_jump daohero_run daohero_run_left daohero_run_right')
+       }
+    });
+
+  } // end Game()
 
 })
