a52035363d824e6d912c1449ff44e46ffb9696ac
Alex Shink <alshinkareff@ya.ru>
2018-05-15 01:26:44 +0700

    websocket moveX

--- resources/common.js
+++ resources/common.js
@@ -6,74 +6,57 @@ $(document).ready(function(){
   ws.onopen = function(){
     console.log('Соединение установлено');
     ws.send('reset');
-    Game();
   };
 
   ws.onmessage = function(e){
-    console.log('Получены данные: ' + e.data);
+    console.log('Получаем reset: ' + e.data);
     wsData = JSON.parse(e.data);
-    // reset
-    // $('.daohero').css('left', wsData.x)
+    $('.daohero').css('left', wsData.player.x);
+    Game();
     console.log('Игра запущена');
   };
 
   function Game(){
 
-    var moveKeys = [37, 38, 39, 40];
-
-    function move(eClass, distance, jump){
-
-    // ABSOLUTE MOVE
-      if ( jump !== undefined && $('.' + eClass).css('bottom').replace('px', '') == 0) {
-        $('.' + eClass)
-          .addClass('daohero_jump')
-          .css('bottom', '+=' + distance);
-        setTimeout(function(){
-          $('.' + eClass).css('bottom', '0' )
-        }, 200)
-      } else if ( keyPressed == 39 ) {
-        $('.' + eClass).addClass('daohero_run');
-        $('.' + eClass).css('left', distance);
-        $('.' + eClass).addClass('daohero_run_right');
-      } else if ( keyPressed == 37 ) {
-        $('.' + eClass).addClass('daohero_run');
-        $('.' + eClass).css('left', distance);
-        $('.' + eClass).addClass('daohero_run_left');
-      }
+    ws.onmessage = function(e){
+      wsData = JSON.parse(e.data);
+      $('.daohero').animate({left: wsData.player.x}, 200, 'linear');
+      console.log(wsData.player.x)
     }
 
-    $(document).bind('keydown', function(e){
+    function Character(movespeed){
 
-      ws.onmessage = function(e){
-        wsData = JSON.parse(e.data);
-        console.log(wsData.X)
-        if ( keyPressed == 39 || keyPressed == 37 ) {
-          move('daohero', wsData.X);
+      this.speed = movespeed;
+      var player = this;
+      var lastMove = new Date();
+      
+      this.move = function(direction){
+        var newMove = new Date();
+        if ( (newMove - lastMove) / 1000 > 1/player.speed ){ 
+          ws.send(direction);
+          lastMove = newMove;
         }
-        setTimeout(function(){
-          $('.daohero').removeClass('daohero_jump daohero_run daohero_run_left daohero_run_right')
-        }, 200)
-      }
-
-      if ( $.inArray(e.keyCode, moveKeys) !== -1 ) {
-        
-        window.keyPressed = e.keyCode;
-
-        switch(keyPressed) { 
-          case 39:
-            ws.send('right');
-            break
-
-          case 37:
-            ws.send('left');
-            break
-
-          case 38:
-            move('daohero', 60, 1);
-            break
+      };
+    }
+    var initMove = function(){
+      $(document).keydown(function(e){ 
+        if ( e.keyCode == 39 || e.keyCode == 37 ) {
+          switch(e.keyCode){
+            case 39:
+              daohero.move('right');
+            break;
+            case 37:
+              daohero.move('left');
+            break;
+          }
         }
-      }
-    });
+      });
+      
+      daohero = new Character(5);
+
+    };                                
+    
+    initMove();
 
   } // end Game()
 
